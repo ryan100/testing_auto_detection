209ff1a2338a8769c02e07f2866f64e17b2e3f78
SAURABH <ryan.saurabh@gmail.com>
2018-08-31 22:18:09 +0530

    Wip : this module will work i suppose
    Signed-off-by: SAURABH <ryan.saurabh@gmail.com>

--- task_05/kern_module.c
+++ task_05/kern_module.c
@@ -13,6 +13,18 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("SAURABH");
 MODULE_DESCRIPTION("HELLO WORLD!!!!");
 
+static const struct usb_device_id id_table[] = {
+	/*
+	 * USB_INTERFACE_INFO(class, subclass, protocol)
+	 */
+	{USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+		USB_INTERFACE_SUBCLASS_BOOT,
+		USB_INTERFACE_PROTOCOL_KEYBOARD)},
+	{}
+};
+
+MODULE_DEVICE_TABLE(usb, id_table);
+
 
 static int __init init_mod(void)
 {
